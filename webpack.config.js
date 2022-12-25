0367052a2b371372f3d3a0339cd7fa046fbd2a39
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2022-12-25 12:07:55 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,33 @@ module.exports = {
     static: path.resolve(__dirname, 'dist'),
     port: 8080,
     hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
   }
 }
