960a390410ee090ace28d6fb8c184a193039179b
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2023-01-10 20:18:26 +0300

    Create ru.js

---
+++ src/js/locales/ru.js
@@ -0,0 +1,20 @@
+export default {
+    translation: {
+      errors: {
+        notValidUrlFormat: 'Ссылка должна быть валидным URL',
+        rssRepeat: 'RSS уже существует',
+        notHaveValidRss: 'Ресурс не содержит валидный RSS',
+        networkProblems: 'Ошибка сети',
+        unknown: 'Что то пошло не так',
+      },
+      watching: 'Идет загрузка RSS',
+      rssAdded: 'RSS успешно загружен',
+      titles: {
+        feeds: 'Фиды',
+        posts: 'Посты',
+      },
+      button: {
+        view: 'Просмотр',
+      },
+    },
+  };
