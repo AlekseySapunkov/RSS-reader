0197b95aff3d550054a691139c959d482498583a
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2023-01-15 18:53:11 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,52 @@
-const path = require('path')
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-module.exports = {
-  entry: './src/js/main.js',
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
+  entry: './src/index.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    port: 8080,
-    hot: true
+    open: true,
+    host: 'localhost',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
   module: {
     rules: [
+      // {
+      // test: /\.(js|jsx)$/i,
+      // loader: 'babel-loader',
+      // },
       {
-        test: /\.(scss)$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: () => [
-                  require('autoprefixer')
-                ]
-              }
-            }
-          },
-          {
-            loader: 'sass-loader'
-          }
-        ]
-      }
-    ]
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+
+    // Add your rules for custom modules here
+    // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
   },
-  mode: 'development'
-}
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+  }
+  return config;
+};
