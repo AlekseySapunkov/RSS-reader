7043933196e2a29201dddf4efe9ecf4de1811b97
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2023-01-20 17:42:09 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ const isProduction = process.env.NODE_ENV === 'production';
 const config = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
