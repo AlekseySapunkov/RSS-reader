e9410603525d2b74fb3e485213438bdaf3e7726b
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2023-01-30 20:21:58 +0300

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,4 @@
 import uniqueId from 'lodash/uniqueId';
-import axios from 'axios';
 
 const parser = (state, data, type, curFeedId) => {
   try {
@@ -49,15 +48,4 @@ const parser = (state, data, type, curFeedId) => {
   }
 };
 
-const tracking = (state, url, i18n, feedId) => {
-  const modifiedUrl = `${i18n.t('proxy')}${encodeURIComponent(url)}`;
-  const iter = () => {
-    axios.get(modifiedUrl)
-      .then((response) => parser(state, response.data, 'existing', feedId))
-      .catch((err) => console.error(err))
-      .then(() => setTimeout(() => iter(), 5000));
-  };
-  setTimeout(() => iter(), 5000);
-};
-
-export { parser, tracking };
+export default parser;
