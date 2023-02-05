0d3e074eab72640a58cb9246f4ad53d2ac6ab9d9
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2023-02-05 19:35:06 +0300

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,50 +1,49 @@
 import uniqueId from 'lodash/uniqueId';
 
 const parser = (state, data, type, curFeedId) => {
-  try {
-    const domParser = new DOMParser();
-    const document = domParser.parseFromString(data.contents, 'text/xml');
-    const items = document.querySelectorAll('item');
-    if (type === 'new') {
-      const chaTitle = document.querySelector('channel > title').textContent;
-      const chaDescription = document.querySelector('channel > description').textContent;
-      state.feeds.push({
-        id: curFeedId, title: chaTitle, description: chaDescription,
-      });
+  const domParser = new DOMParser();
+  const document = domParser.parseFromString(data.contents, 'text/xml');
+  const items = document.querySelectorAll('item');
+  const parseError = document.querySelector('parsererror');
+  if (parseError) {
+    throw new Error(parseError.textContent);
+  }
+  if (type === 'new') {
+    const chaTitle = document.querySelector('channel > title').textContent;
+    const chaDescription = document.querySelector('channel > description').textContent;
+    state.feeds.push({
+      id: curFeedId, title: chaTitle, description: chaDescription,
+    });
 
-      items.forEach((item) => {
-        const title = item.querySelector('title').textContent;
-        const description = item.querySelector('description').textContent;
-        const link = item.querySelector('link').textContent;
-        const postId = uniqueId();
-        state.posts.push({
-          feedId: curFeedId, id: postId, title, description, link,
-        });
+    items.forEach((item) => {
+      const title = item.querySelector('title').textContent;
+      const description = item.querySelector('description').textContent;
+      const link = item.querySelector('link').textContent;
+      const postId = uniqueId();
+      state.posts.push({
+        feedId: curFeedId, id: postId, title, description, link,
       });
-    }
-    if (type === 'existing') {
-      const existingPosts = state.posts.filter(({ feedId }) => feedId === curFeedId);
-      const existingPostsTitles = existingPosts.map(({ title }) => title);
-      const newPosts = Array.from(items).filter((item) => {
-        const title = item.querySelector('title').textContent;
-        return !existingPostsTitles.includes(title);
+    });
+  }
+  if (type === 'existing') {
+    const existingPosts = state.posts.filter(({ feedId }) => feedId === curFeedId);
+    const existingPostsTitles = existingPosts.map(({ title }) => title);
+    const newPosts = Array.from(items).filter((item) => {
+      const title = item.querySelector('title').textContent;
+      return !existingPostsTitles.includes(title);
+    });
+    newPosts.forEach((post) => {
+      const title = post.querySelector('title').textContent;
+      const description = post.querySelector('description').textContent;
+      const link = post.querySelector('link').textContent;
+      const postId = uniqueId();
+      state.trackingPosts.push({
+        feedId: curFeedId, id: postId, title, description, link,
       });
-      newPosts.forEach((post) => {
-        const title = post.querySelector('title').textContent;
-        const description = post.querySelector('description').textContent;
-        const link = post.querySelector('link').textContent;
-        const postId = uniqueId();
-        state.trackingPosts.push({
-          feedId: curFeedId, id: postId, title, description, link,
-        });
-        state.posts.push({
-          feedId: curFeedId, id: postId, title, description, link,
-        });
+      state.posts.push({
+        feedId: curFeedId, id: postId, title, description, link,
       });
-    }
-  } catch (err) {
-    state.parsingErrors.push(err);
-    throw new Error();
+    });
   }
 };
 
